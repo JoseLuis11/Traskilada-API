7bb6ed6f8eb70360e8fe7307fd5921166ef1f94e
JoseLuis <jluis.scooter11@hotmail.com>
2017-05-06 01:25:29 -0700

    Account change

--- TraskiladaWebAPI/src/TraskiladaWebAPI/Models/Account.cs
+++ TraskiladaWebAPI/src/TraskiladaWebAPI/Models/Account.cs
@@ -2,7 +2,6 @@ namespace TraskiladaWebAPI.Models
 {
     public class Account : BaseModel
     {
-        public int Id { get; set; }
         public string Email { get; set; }
         public string Password {get ; set; }
         public User User {get; set; }
