71ce8f4d8d3ce4effa681ee228cae280340ad1fa
JoseLuis <jluis.scooter11@hotmail.com>
2017-05-06 02:54:11 -0700

    ClientController

---
+++ TraskiladaWebAPI/src/TraskiladaWebAPI/Controllers/ClientController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using TraskiladaWebAPI.Models;
+
+namespace TraskiladaWebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    public class ClientController : Controller
+    {
+
+        // GET api/values
+        [HttpGet]
+        public IEnumerable<ClientController> Get()
+        {
+            return null;
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public ClientController Get(int id)
+        {
+            return null;
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody]ClientController value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]ClientController value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+
+    }
+}
