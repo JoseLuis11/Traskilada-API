1b1837a5353be780bf781ec512f14ec2142c847b
JoseLuis <jluis.scooter11@hotmail.com>
2017-05-06 02:09:43 -0700

    String changes

--- TraskiladaWebAPI/src/TraskiladaWebAPI/Controllers/EmployeeController.cs
+++ TraskiladaWebAPI/src/TraskiladaWebAPI/Controllers/EmployeeController.cs
@@ -11,27 +11,27 @@ namespace TraskiladaWebAPI.Controllers
     {
         // GET api/employee
         [HttpGet]
-        public IEnumerable<string> Get()
+        public IEnumerable<Employee> Get()
         {
-            return new string[] { "value1", "value2" };
+            return null;
         }
 
         // GET api/values/5
         [HttpGet("{id}")]
-        public string Get(int id)
+        public Employee Get(int id)
         {
-            return "value";
+            return null;
         }
 
         // POST api/values
         [HttpPost]
-        public void Post([FromBody]string value)
+        public void Post([FromBody]Employee value)
         {
         }
 
         // PUT api/values/5
         [HttpPut("{id}")]
-        public void Put(int id, [FromBody]string value)
+        public void Put(int id, [FromBody]Employee value)
         {
         }
 
