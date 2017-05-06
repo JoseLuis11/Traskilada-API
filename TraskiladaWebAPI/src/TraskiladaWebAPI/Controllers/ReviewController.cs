2fdec86e41c61d3d7ff7fe02105569ee9b4f8240
JoseLuis <jluis.scooter11@hotmail.com>
2017-05-06 04:20:06 -0700

    Review Controller Methods

--- TraskiladaWebAPI/src/TraskiladaWebAPI/Controllers/ReviewController.cs
+++ TraskiladaWebAPI/src/TraskiladaWebAPI/Controllers/ReviewController.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using TraskiladaWebAPI.Models;
 using TraskiladaWebAPI.Context;
+using System.Threading;
 
 namespace TraskiladaWebAPI.Controllers
 {
@@ -21,34 +22,37 @@ namespace TraskiladaWebAPI.Controllers
 
         // GET api/values
         [HttpGet]
-        public IEnumerable<Review> Get()
+        public async Task<IEnumerable<Review>> Get()
         {
-            return null;
+            return await _context.ReviewRepository.GetAllAsync(CancellationToken.None);
         }
 
         // GET api/values/5
         [HttpGet("{id}")]
-        public Review Get(int id)
+        public async Task<Review>Get(int id)
         {
-            return null;
+            return await _context.ReviewRepository.FindAsync(CancellationToken.None);
         }
 
         // POST api/values
         [HttpPost]
         public void Post([FromBody]Review value)
         {
+            _context.ReviewRepository.AddAsync(value, CancellationToken.None);
         }
 
         // PUT api/values/5
         [HttpPut("{id}")]
         public void Put(int id, [FromBody]Review value)
         {
+            _context.ReviewRepository.AddAsync(value,CancellationToken.None);
         }
 
         // DELETE api/values/5
         [HttpDelete("{id}")]
         public void Delete(int id)
         {
+            _context.ReviewRepository.RemoveById(id);
         }
 
     }
