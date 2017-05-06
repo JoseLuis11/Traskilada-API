912930808735c00a411cf135658a78abb060efa1
JoseLuis <jluis.scooter11@hotmail.com>
2017-05-06 04:11:18 -0700

    UserController

--- TraskiladaWebAPI/src/TraskiladaWebAPI/Controllers/UserController.cs
+++ TraskiladaWebAPI/src/TraskiladaWebAPI/Controllers/UserController.cs
@@ -5,50 +5,54 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using TraskiladaWebAPI.Models;
 using TraskiladaWebAPI.Context;
+using System.Threading;
 
 namespace TraskiladaWebAPI.Controllers
 {
     [Route("api/[controller]")]
-    public class ClientController : Controller
+    public class UserController : Controller
     {
 
         private readonly TraskiladaContext _context;
 
-        public ClientController(TraskiladaContext context)
+        public UserController(TraskiladaContext context)
         {
             _context = context;
         }
 
         // GET api/values
         [HttpGet]
-        public IEnumerable<User> Get()
+        public async Task<IEnumerable<User>> Get()
         {
-            return null;
+            return await _context.UserRepository.GetAllAsync(CancellationToken.None);
         }
 
         // GET api/values/5
         [HttpGet("{id}")]
-        public User Get(int id)
+        public async Task<User> Get(int id)
         {
-            return null;
+            return await _context.UserRepository.FindAsync(CancellationToken.None);
         }
 
         // POST api/values
         [HttpPost]
         public void Post([FromBody]User value)
         {
+            _context.UserRepository.AddAsync(value, CancellationToken.None);
         }
 
         // PUT api/values/5
         [HttpPut("{id}")]
         public void Put(int id, [FromBody]User value)
         {
+            _context.UserRepository.Update(value);
         }
 
         // DELETE api/values/5
         [HttpDelete("{id}")]
         public void Delete(int id)
         {
+            _context.UserRepository.RemoveById(id);
         }
 
     }
