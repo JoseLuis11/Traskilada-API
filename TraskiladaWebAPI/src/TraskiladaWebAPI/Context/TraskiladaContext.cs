ef27086a83a29364d27bf8d0fd6331e7de064a8a
hectore.hernandez <hectore.hernandez@LSTKEN48215>
2017-05-06 02:10:22 -0700

    Account login validation

--- TraskiladaWebAPI/src/TraskiladaWebAPI/Context/TraskiladaContext.cs
+++ TraskiladaWebAPI/src/TraskiladaWebAPI/Context/TraskiladaContext.cs
@@ -1,11 +1,13 @@
 using Microsoft.EntityFrameworkCore;
+using TraskiladaWebAPI.Models;
 using TraskiladaWebAPI.Repositories;
 
 namespace TraskiladaWebAPI.Context
 {
     public class TraskiladaContext : DbContext
     {
-        // private IGenericRepository<Person> _personRepository;
+        //private IGenericRepository<Person> _personRepository;
+        private IAccountRepository _accountRepository; 
 
         public TraskiladaContext(DbContextOptions options) : base(options)
         {
@@ -15,8 +17,12 @@ namespace TraskiladaWebAPI.Context
         // public IGenericRepository<Person> PersonRepository =>
         //     _personRepository ?? (_personRepository = new GenericRepository<Person>(Persons));
 
+        public IAccountRepository AccountRepository => _accountRepository ?? (_accountRepository = new AccountRepository(Accounts));
+
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
         }
+
+        public DbSet<Account> Accounts { get; set; }
     }
 }
---
+++ TraskiladaWebAPI/src/TraskiladaWebAPI/Repositories/AccountRepository.cs
@@ -0,0 +1,22 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using TraskiladaWebAPI.Models;
+
+namespace TraskiladaWebAPI.Repositories
+{
+    public interface IAccountRepository
+    {
+        Task<Account> ValidateCredentials(Account account);
+    }
+
+    public class AccountRepository : GenericRepository<Account>, IAccountRepository
+    {
+        public AccountRepository(DbSet<Account> dbSet) : base(dbSet)
+        {
+        }
+
+        public async Task<Account> ValidateCredentials(Account account) =>
+            await SingleAsync(x => x.Email == account.Email && x.Password == account.Password, CancellationToken.None);
+    }
+}
