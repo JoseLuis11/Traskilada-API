ecc71af11913b47942781acc57b8d2fd20a8097d
J. Pichardo <pichardo.software@gmail.com>
2017-05-05 23:52:44 -0700

    Person Model

---
+++ TraskiladaWebAPI/src/TraskiladaWebAPI/Models/Person.cs
@@ -0,0 +1,11 @@
+namespace TraskiladaWebAPI.Models
+{
+    public class Person
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Email { get; set; }
+    }
+}
